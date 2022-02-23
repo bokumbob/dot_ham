a64924586b6299094dcf34ff6b255911c8ef2495
DESKTOP-SPJEOPE\user <jjs327020@gmail.com>
2022-02-23 18:54:36 +0900

    출력이안되는데 수정좀

--- src/pages/home/Main.js
+++ src/pages/home/Main.js
@@ -1,45 +1,24 @@
-// import React, { useEffect } from 'react'
 import './main.scss'
 import { useState } from 'react'
 import Timer from 'component/main/Timer'
-// import classNames from 'classnames'
 import hamsterList from 'model/hamsterList'
-import collectedHamsterList from 'model/hamsterCollection'
 
 
 export default function Main() {
-    // console.log(collectedHamsterList)
-    
-    // const hamster = document.querySelector(".hamster")
     const hamsterState = { show: false, name: "", id: "", activeStatus: false }
     const [hamsterShow, setHamsterShow] = useState(false);
     const [hamsterActiveS, setHamsterActiveS] = useState(false);
     const [activeTime, setActiveTime] = useState("");
-    const [hamsterId, setHamsterId] = useState("");
-    const [unidentifiedList, setUnidentifiedList] = useState(hamsterList.allHamsterLists)
-    // console.log(hamsterList.allHamsterLists)
-    // console.log(unidentifiedList.filter(item => item.id === hamsterId))
-    
-    let hamsterIdMaker;
+    const [currentNum, setCurrentNum] = useState('')
+    let [hamsterCollection, setHamsterCollection] = useState()
 
     const hamActive = (time)=>{
         setActiveTime(time)
         if(activeTime <=0){
-            // const seeds = document.querySelector(".seeds");
-            // seeds.classList.add("active")
-            // console.log("hamactive")
             setHamsterActiveS(true)
             stateAssign({activeStatus : hamsterActiveS})
         }
     }
-
-    // useEffect(()=>{
-    //     if(activeTime < 0){
-    //      const seeds = document.querySelector(".active");
-    //     //  console.log("hamactive")
-    //     //  console.log(activeTime)
-    //         }
-    //     }, [activeTime])
 
     const catchHamster = () => {
         if(activeTime<0){
@@ -48,29 +27,27 @@ export default function Main() {
             setHamsterActiveS(false)
             stateAssign({activeStatus : hamsterActiveS})
 
-            // collectedHamsterList
             localStorage.setItem("hamster", "hamham")
-            hamsterIdMaker = unidentifiedList.filter(item => item.id === localStorage.getItem("hamsterId"))
-            if(!localStorage.getItem("hamsterId")){
-                localStorage.setItem("hamsterId", 0)
-                setUnidentifiedList(unidentifiedList.filter(item => item.id !== localStorage.getItem("hamsterId")))
-            }
-            // localStorage.setItem("hamsterId", unidentifiedList[Math.floor(Math.random() * unidentifiedList.length)].id)
-            // setHamsterId(localStorage.getItem("hamsterId"))
-            // console.log(unidentifiedList)
-            // console.log(hamsterId)
-            // console.log(hamsterActiveS)
-        }
-        //  else if(hamsterActiveS === false){
-        //     setHamsterShow(false)
-        //     stateAssign({show : hamsterShow})
-        // }
-    }
 
-    // const timeReset = (resetTime)=>{
-    //         setActiveTime(+resetTime)
-    //         console.log(activeTime+"액티브타이")
-    // }
+            if(!localStorage.getItem("getHamList")){
+                localStorage.setItem("0","0");
+                const notD = hamsterList.allHamsterLists.filter(item => item.id !== '0')
+                setHamsterCollection(hamsterCollection = notD)
+                const StrHamsterCollection = JSON.stringify(hamsterCollection)
+                localStorage.setItem("getHamList", StrHamsterCollection)
+
+            } else if (localStorage.getItem("getHamList")){
+                const prevHamsterList = JSON.parse(localStorage.getItem("getHamList"))
+                const currentHamsterArr = prevHamsterList[Math.floor(Math.random() * prevHamsterList.length)]
+                const currentHamsterList = prevHamsterList.filter(item => item.id !== currentHamsterArr.id) 
+                setHamsterCollection(hamsterCollection = currentHamsterList)
+                const StrHamsterCollection = JSON.stringify(hamsterCollection)
+                localStorage.setItem("getHamList", StrHamsterCollection)
+                localStorage.setItem(currentHamsterArr.id, currentHamsterArr.id)
+                setCurrentNum(currentHamsterArr.id)
+            }
+        }
+    }
 
     function stateAssign(newState){
         Object.assign( hamsterState, newState)
@@ -85,20 +62,19 @@ export default function Main() {
         return (
             <div className='background'>
                 <article className='pop'>
-                    {/* <h2>hiddenTitle</h2> */}
-                        {hamsterIdMaker.map((item, index) => {
-                            return (
-                                <div className='your-hamster' key={index}>
-                                    <p>{item.name}</p>
-                                    <p>{item.description}</p>
-                                    <img src = {item.imgUrl} alt={item.name} />
-                                </div>
-                            )
+                    <h2>hiddenTitle</h2>
+                    {/* {hamsterCollection[0].name} */}
+                        {hamsterList.allHamsterLists.map((item, index) =>  {
+                            item.id == currentNum.id && 
+                             <div className='your-hamster' key={index}>
+                                        <p>{item.name}</p>
+                                        <p>{item.description}</p>
+                                        <img src = {item.imgUrl} alt={item.name} />
+                                    </div>
                         })}
                 </article>
                 <p>앗, 야생의 햄스터가 나타났다!</p>
                 <div className="pop-x" onClick={popClose}><p>x</p></div>
-                {/* onClick={timeReset} */}
             </div>
         )
     }
@@ -107,8 +83,6 @@ export default function Main() {
         return (
             <>
                 <article className={hamsterActiveS ? 'seeds active' : 'seeds'} onClick={hamsterActiveS ? catchHamster : null}></article>
-                {/* {classNames('seeds', {active : activeTime < 0})} */}
-                {/* <article className='hamster'></article> */}
             </>
         )
     }
@@ -121,7 +95,6 @@ export default function Main() {
                 <h2 className="hidden">hiddenTitle</h2>
                 <div className="center">
                     <Timer active={hamActive} activeStatus={hamsterActiveS}/>
-                    {/* reset={timeReset} */}
                     {hamsterShow ? <ShowHamster /> : <HideHamster />}
                 </div>
             </section>
